3ae0a9b592ffe6d30853f9b29273511aa6707b4c
Lilly Nguyen <ling3076@colorado.edu>
2024-10-29 10:08:36 -0600

    Add two event listeners to ica10

--- js/wk_10.js
+++ js/wk_10.js
@@ -1,19 +1,14 @@
-let my_var = 100;
-
-var my_other_var = "hello world";
-const constant_var = "constant";
-console.log(my_other_var);
-my_other_var += "!";
-// alert(my_other_var);
-
-// how to interact with the DOM
-console.log(document.getElementById("my_head"));
-
-document.getElementById("my_head").addEventListener("click", function(e){
-    alert("clicked on the h1");
-    document.getElementById("paragraph").innerHTML = "this is the new text";
-    document.getElementById("paragraph").style.color = "blue";
+document.getElementById("button1").addEventListener("click", function(e){
+    // change attributes of element
+    document.getElementById("image").src = "../class_notes/images/unicorns/unicorn (3).png"
 })
-console.log(document.getElementById("my_head").innerHTML);
+
+var block_of_text = "Lorem ipsum odor amet, consectetuer adipiscing elit. Parturient nisi netus magnis urna faucibus nibh adipiscing congue dui. Mus justo sagittis phasellus morbi blandit aliquam justo eros. Varius luctus torquent in potenti sociosqu cras. Mus tincidunt hendrerit dui penatibus phasellus pharetra penatibus morbi. Dictumst auctor eu suscipit praesent vulputate efficitur ut. Quisque dui maecenas urna hendrerit nunc ex. Ultrices fames pellentesque adipiscing curabitur ac nam tempor. Blandit per dolor class vulputate risus etiam elementum inceptos."
+
+document.getElementById("button2").addEventListener("click", function(e){
+    // change attributes of element
+    
+    document.getElementById("paragraph").innerHTML = block_of_text;
+})
 
 // dblclick, mouseleave, mouseover, mousemove
