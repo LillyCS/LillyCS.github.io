1b86595bdf62e0d23c2c32ab61de2d8bd2ccc8d1
Lilly Nguyen <ling3076@colorado.edu>
2024-11-05 10:29:44 -0700

    Write calculateDogAge function

--- js/wk_11.js
+++ js/wk_11.js
@@ -1,5 +1,3 @@
-const story = document.querySelector('.story');
-
 function tellFortune(num_children, partner_name, geo, job, id_) {
     /*
         Parameters: number of children, partner's name, geographic location, job title.
@@ -17,3 +15,22 @@ tellFortune("2", "Ryan", "Hawaii", "Painter", "fortune1");
 tellFortune("10", "Bob", "Utah", "Scientist","fortune2");
 
 tellFortune("5", "Peter", "California", "Astronaut", "fortune3");
+
+function calculateDogAge(age,id_){
+    /*
+        Parameters: your puppy's age.
+        Return calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+        outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
+        Call the function three times with different sets of values.
+    */
+
+    dog_age = age*7;
+    /* using template literal to access age contents */
+    output = `Your doggie is ${dog_age} years old in dog years!`;
+
+    document.getElementById(id_).innerHTML = output;
+}
+
+calculateDogAge(1,"dog1");
+calculateDogAge(2,"dog2");
+calculateDogAge(3,"dog3");
