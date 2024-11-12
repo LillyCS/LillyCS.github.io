ef8e324cb1f6120d8bf33e13dcaf07248e6008d9
Lilly Nguyen <ling3076@colorado.edu>
2024-11-12 09:44:48 -0700

    Add wk12 notes

---
+++ js/wk_12.js
@@ -0,0 +1,8 @@
+let ourObject = {
+    "name": "lilly",
+    "profession": "student",
+    "age": 21,
+    "family": [{"name":"kim", "relation":"mom","age":52},{"name":"kiet", "relation":"data","age":55},{"name":"khai", "relation":"brother","age":27}]
+}
+
+console.log(ourObject);
