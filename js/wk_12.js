217588e8970d760e02e01c728f19f8a0ef4299cf
Lilly Nguyen <ling3076@colorado.edu>
2024-11-12 13:58:58 -0700

    Add js to joke generator

--- js/wk_12.js
+++ js/wk_12.js
@@ -1,8 +1,32 @@
-let ourObject = {
-    "name": "lilly",
-    "profession": "student",
-    "age": 21,
-    "family": [{"name":"kim", "relation":"mom","age":52},{"name":"kiet", "relation":"data","age":55},{"name":"khai", "relation":"brother","age":27}]
-}
+// let ourObject = {
+//     "name": "lilly",
+//     "profession": "student",
+//     "age": 21,
+//     "family": [{"name":"kim", "relation":"mom","age":52},{"name":"kiet", "relation":"data","age":55},{"name":"khai", "relation":"brother","age":27}]
+// }
 
-console.log(ourObject);
+// console.log(ourObject);
+const btn = document.querySelector('button');
+
+let getJoke = fetch(
+    "https://api.chucknorris.io/jokes/random");
+        
+// getJoke is the promise to resolve
+// it by using.then() method
+getJoke.then((res) =>{
+    if(!res.ok) throw new Error(res.status);
+    else return res.json();
+})
+.then(d => {
+        document.getElementById("joke").innerHTML = d.value;
+        console.log("Button was clicked!");
+        
+})
+.catch(error => {
+    // Handle errors
+    console.log(res);
+    console.error('There was a problem with the fetch operation:', error);
+    alert("There was a problem with the fetch operation");
+});
+
+btn.addEventListener("click", getJoke);
