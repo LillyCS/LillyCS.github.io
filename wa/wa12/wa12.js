d8bf41b9896e2e58da66d8bc51b326a11473608b
Lilly Nguyen <ling3076@colorado.edu>
2024-11-14 12:05:00 -0700

    Add javascript to call api and populate page with comic details

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -8,7 +8,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
-document.getElementById("button1").addEventListener("click", function(e){
+document.getElementById("button1").addEventListener("click", async function(e){
     console.log("button was clicked");
 
     try {
@@ -17,10 +17,36 @@ document.getElementById("button1").addEventListener("click", function(e){
 
         let uri = `https://corsproxy.io/?https://xkcd.com/${random_num}/info.0.json`;
         
+        const res = await fetch(uri);
+
+        if(!res.ok){
+            console.log(res);
+            throw new Error(`Response status: ${res.status}`); // error.message
+        }
+        else {
+            // get json data
+            const json = await res.json(); // get the json object
+
+            // gather comic image
+            const comic_img = json.img;
+            document.getElementById("image").src = comic_img;
+
+            // gather comic title
+            const title = json.title;
+            document.getElementById("title").innerHTML = title;
+
+            // gather variables to concatentate publish date of comic
+            const month = json.month;
+            const day = json.day;
+            const year = json.year;
+            const date = month+"/"+day+"/"+year;
+
+            document.getElementById("date").innerHTML = date;
+             
+    }
     } catch (error){
         console.error(error.message);
         alert("There was a problem with the fetch operation.");
     }
-    // change attributes of element
-    // document.getElementById("image").src = "../../class_notes/images/unicorns/unicorn (3).png"
+
 })
