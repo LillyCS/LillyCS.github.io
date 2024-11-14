7ac0769ccdb8654659eff3c671156b23d015ba5e
Lilly Nguyen <ling3076@colorado.edu>
2024-11-14 11:42:28 -0700

    Add button and boilerplate comic generator

---
+++ wa/wa12/wa12.js
@@ -0,0 +1,4 @@
+document.getElementById("button1").addEventListener("click", function(e){
+    // change attributes of element
+    document.getElementById("image").src = "../../class_notes/images/unicorns/unicorn (3).png"
+})
